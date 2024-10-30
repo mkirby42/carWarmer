b0c713c2ecf7ca11b77e4a53dbaaa4919f776a7f
mkirby42 <6mdkirby@gmail.com>
2024-10-30 16:14:15 -0600

    Initial Commit

---
+++ CarWarmeriOSTests/CarWarmeriOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  CarWarmeriOSTests.swift
+//  CarWarmeriOSTests
+//
+//  Created by Mathew Kirby on 10/30/24.
+//
+
+import Testing
+@testable import CarWarmeriOS
+
+struct CarWarmeriOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
